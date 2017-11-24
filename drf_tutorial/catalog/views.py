4e1f1f67ad87ef9fd9a4ae29b02ce738c17319f9
lgants <lgants@gmail.com>
2017-11-23 22:55:27 -0600

    switched to APIView

--- drf_tutorial/catalog/views.py
+++ drf_tutorial/catalog/views.py
@@ -1,4 +1,27 @@
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import status
+
+class ProductList(APIView):
+    # Unlike ListAPIView, must explicitly define the GET method
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+"""
+from django.http import HttpResponse
 from rest_framework import generics
 from rest_framework.response import Response
 from .models import Product
@@ -8,11 +31,11 @@ from .serializers import ProductSerializer
 class ProductList(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
+"""
 
 
 """
-# Deprecated in favor of ListAPIView
+# Deprecated in favor of APIView
 
 from django.http import HttpResponse
 from rest_framework.views import APIView
