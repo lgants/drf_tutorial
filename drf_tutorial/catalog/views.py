3decb8d2ff8192bbf81399188b105b0e1d7ddc3f
lgants <lgants@gmail.com>
2017-11-23 22:43:53 -0600

    updated views

--- drf_tutorial/catalog/views.py
+++ drf_tutorial/catalog/views.py
@@ -1,20 +1,29 @@
 from django.http import HttpResponse
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import generics
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
 
-class JSONResponse(HttpResponse):
-    # An HttpResponse that renders its content into JSON
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
-def product_list(request):
-    if request.method == 'GET':
+
+"""
+# Deprecated in favor of ListAPIView
+
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductList(APIView):
+    def get(self, request, format=None):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
+"""
